fbe0dc2bee0513488b77724611a281f26f33a262
Anu Sharma <anulepau@gmail.com>
2023-01-14 20:38:57 -0800

    small changes, basic code

--- src/components/Artifact.svelte
+++ src/components/Artifact.svelte
@@ -1,18 +1,10 @@
 <script>
-	export let left = 30;
-	export let top = 30;
+	export let left = 20;
+	export let top = 20;
 	export let width = 100;
 	export let height = 100;
 	
 	let moving = false; 
-	
-	function start() {
-		moving = true;
-	}
-	
-	function stop() {
-		moving = false;
-	}
 	
 	function move(e) {
 			if (moving) {
@@ -22,9 +14,9 @@
 	}
 </script>
 
-<svelte:window on:mouseup={stop} on:mousemove={move}  />
+<svelte:window on:mouseup={() => moving=false} on:mousemove={move}  />
 
-<section on:mousedown={start} style="left: {left}px; top: {top}px; width:{width}px; height:{height}px" class="item">
+<section on:mousedown={() => moving=true} style="left: {left}px; top: {top}px; width:{width}px; height:{height}px" class="item">
 	<slot></slot>
 </section>
 
