f4b8cd99ce3a631ba4c151a31ee3866fd1035906
Anu Sharma <anulepau@gmail.com>
2023-01-14 19:47:39 -0800

    working svg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,16 @@
 <script>
   import Item from '../components/Item.svelte';
-  import GoodNightWP from '../assets/GoodNightWP.svg';
-  import SmashFacism from '../assets/SmashFacism.svg';
+	import Couch from '../assets/couch.svg';
 </script>
 
-<Item top={50} left={100} width={200} height={300}>
-	<img src={GoodNightWP} alt='good night white pride'>
-</Item>
-
-<Item top={20} left={20} width={50} height={50} >
-	<img src={SmashFacism} alt='smash facism'>
+<Item top={20} left={20} width={100} height={100} >
+	<img src={Couch} alt='plushy neon pink couch'>
 </Item>
 
 <style>
 	img {
 		user-select: none;
 		pointer-events: none;
-		object-fit: scale-down;
+		max-width: 100px;
 	}
 </style>
